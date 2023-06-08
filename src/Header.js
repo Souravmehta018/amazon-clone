9637cf56f9ff3954dfe115caa33929576ba4d5e9
Sourav <mehtas018@gmail.com>
2023-06-08 23:12:06 +0530

    Header for amazon clone

---
+++ src/Header.js
@@ -0,0 +1,53 @@
+/* To make things faster we added extension es7 
+wrote "rfce" and basic format automatially popped up*/
+import React from 'react';
+import "./Header.css";
+import SearchIcon from '@mui/icons-material/Search'; /*copied from material ui website to download search icon */
+import { ShoppingCartCheckout } from '@mui/icons-material';
+
+function Header() {
+  return (
+    <div className='header'>
+      <img className='header__logo'
+      src='https://nickiekrommingahill.com/wp-content/uploads/2020/05/white-amazon-logo-png-6.png' 
+      />
+      <div className='header__search'>
+        <input className='header__searchInput' type='text' />      
+        <SearchIcon className='header__searchIcon' />
+      </div>
+
+      <div className='header__nav'>
+        <div className='header__options'>
+          <span className='header__optionLineOne'>
+            Hello Guest
+          </span>
+          <span className='header__optionLineTwo'>
+            Sign In
+          </span>
+        </div>
+        <div className='header__options'>
+        <span className='header__optionLineOne'>
+          Returns
+          </span>
+          <span className='header__optionLineTwo'>& Orders </span>
+        </div>
+        <div className='header__options'>
+        <span className='header__optionLineOne'>
+          Your
+          </span>
+          <span className='header__optionLineTwo'>
+            Prime
+          </span>
+        </div>
+        <div className="header__optionBasket">
+          <ShoppingCartCheckout />
+          <span className='header__optionBasket header__basketCount'>
+            0
+          </span>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Header
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyArmxptiZg0F3Euia3Zd-WvgfjYzoUQec0",
+  authDomain: "clone-72a7c.firebaseapp.com",
+  projectId: "clone-72a7c",
+  storageBucket: "clone-72a7c.appspot.com",
+  messagingSenderId: "270912135124",
+  appId: "1:270912135124:web:ed2534c2216bc55cdf0041",
+  measurementId: "G-7WDPEXJ1QQ"
+};
